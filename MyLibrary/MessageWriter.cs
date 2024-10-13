1f56f531c1cc248634d820756aa60de777707bd8
Giovanni Saavedra Valdes <giovannisaavedra160@gmail.com>
2024-10-12 21:53:11 -0500

    Commit

---
+++ MyLibrary/MessageWriter.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary
+{
+    public class MessageWriter
+    {
+        public void Writer(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- MyconsoleApp/Program.cs
+++ MyconsoleApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyLibrary;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Writer("Hello, Writer!");
+
+            MessageWriter FWriter = new MessageWriter();
+            FWriter.Writer("Hello, Figgle!!!");
+        }
+    }
+}
